b98a03becce9b812eef7056305eaf7ecc4433aff
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-04-14 19:34:34 +0500

    add index and app.js file

---
+++ app.js
@@ -0,0 +1,113 @@
+// Q1
+// let integerNumber = Number( prompt("Enter the positive Number for display Math method/Data type"));
+
+
+// if(integerNumber>0){
+// 	document.write(typeof(integerNumber)+ ": "+ integerNumber   +"<br>")
+// 	let round = Math.round(integerNumber)  ;
+// 	document.write("round off value: "+ round +"<br>","floor value: " + Math.floor(integerNumber) +"<br>" , "ceil value: " + Math.ceil(integerNumber));
+
+// }
+
+
+
+// else{
+// 	alert("Please! enter the positive number")
+// }
+
+
+// Q2
+// let negativeInteger = parseFloat (prompt("Enter the negative for display Math method/datatype"))
+
+// if(negativeInteger<0){
+
+
+// 	document.write(typeof(negativeInteger) +": " +negativeInteger  +"<br>");
+// 	document.write("round off value: " + Math.round(negativeInteger)+"<br>","floor value: " + Math.floor(negativeInteger)  +"<br>","ceil value: "+ Math.ceil(negativeInteger) )
+
+
+// }else{
+// 	alert("Please enter the negative number")
+// }
+
+
+// Q3
+// let absolute = prompt("Enter the number for absolute")	
+// document.write(absolute)
+
+
+//Q4
+// let dice = Math.round(Math.random()*10) 
+//  document.write("random dice value: "+ dice  +"<br>"    )
+// let dice1 = Math.round(Math.random()*10)
+// document.write("random dice value: "+ dice1  +"<br>")
+
+
+//Q6
+// let random = Math.round(Math.random()*100);
+
+// document.write("The random number from 1 to 100: "+ "<b>"  +random  + "<b>")
+
+
+// Q6
+//  let user =parseFloat( prompt("Enter a number between 1 and 10"))
+// let secretNumber = Math.round( Math.random()*10)
+
+
+
+// if(user===secretNumber){
+// 	document.write("Congratulation! You enter the same number "+ secretNumber +" === " + user )
+// }else{
+// 	alert("You enter the wrong number.try aganin")
+// }
+
+
+// Q7
+// let weihgt =  prompt("Enter your weight in kilograms")
+
+// if(weihgt<= 150 ){
+// 	document.write("The weight of the user is "  +  weihgt)
+// }
+
+
+// else{
+// 	alert("Are you sure your weight is " + weihgt + "you should maintian the proper weight")
+// }
+
+// Q
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
